0941acc90e639fa3f2a81dd3a47fba1a567d5261
SolKacil <oscarisaisd@gmail.com>
2025-09-28 14:13:20 -0600

    Fix DH function and DH main

--- FKsym.m
+++ FKsym.m
@@ -1,21 +1,27 @@
-%Este programa calcula la cinematica de un robot
+%This program calculates the forward kinematics of a robot using
+%the Denavit-Hartenberg convention. It uses symbolic variables to
+%represent joint angles and link lengths, allowing for general solutions.
+%You can modify the symbolic variables to number values to evaluate the
+%kinematics for a specific configuration.
+
 clear all
 close all
 clc
 
-syms d1 d2 d3 d4 a0 a1 a2 a3 q1 q2 q3
-a = transpose([q1 q2 q3]);
-       %q d a alpha
-DHs = [q1 0 a1 0,
+%Define symbolic variables for joint angles and link lengths
+syms d1 d2 d3 a0 a1 a2 a3 q1 q2 q3
+
+%you can define the variables as numbers to evaluate the kinematics for a specific configuration
+a_1=1; a_2=1; q_1=pi/4; q_2=pi/4; 
+
+%the order of the DH parameters is: q d a alpha
+DH_s = [q1 0 a1 0,
        q2 0 a2 0];
 
-DHs2 = [0 0 0 0,
-       q1 0 a1 0
-       q2  0 a2 0];
+DH_n = [q_1 0 a_1 0
+       q_2  0 a_2 0];
 
-FKs = simplify(forwardKinematicsDH(DHs, 2))
-FKs2 = simplify(forwardKinematicsDH(DHs2, 3))
-xyz = FKs(:,4)
-
-
-
+FK_s = simplify(forwardKinematicsDH(DH_s, 2))
+FK_n = forwardKinematicsDH(DH_n, 2)
+xyz_s = FK_s(:,4)
+xyz_n = FK_n(:,4)
--- forwardKinematicsDH.m
+++ forwardKinematicsDH.m
@@ -1,5 +1,10 @@
+% forwardKinematicsDH.m
+% This function computes the forward kinematics of a robot using the
+% Denavit-Hartenberg convention. It takes a matrix of DH parameters and the
+% number of joints (m) as inputs and returns the homogeneous transformation
+% matrix representing the end-effector pose.
+
 function [H] = forwardKinematicsDH(DH, m)
-    %[m, n] = size(DH);
     H = eye(4);
     for i = 1 : m
         H = H * (Rz(DH(i, 1)) * Tz(DH(i, 2)) * Tx(DH(i, 3)) * Rx(DH(i, 4)));
